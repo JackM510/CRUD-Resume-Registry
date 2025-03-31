02397f8e6a6c6810dff7858e5e05f3032e631a12
JackM510 <37175625+JackM510@users.noreply.github.com>
2025-04-01 09:42:09 +1100

    Update pdo.php
    Added missing '?>' closing php tag

--- pdo.php
+++ pdo.php
@@ -4,3 +4,4 @@ $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc2',
    'jack', 'example');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
